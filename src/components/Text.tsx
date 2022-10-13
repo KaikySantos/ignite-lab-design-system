1b270a6c95450028caac57bfeef14f283da66def
kaikySantos <info.kaiky@gmail.com>
2022-10-13 12:10:17 -0300

    feat: form

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -6,10 +6,11 @@ export interface TextProps {
   size?: 'sm' | 'md' | 'lg'
   children: ReactNode
   asChild?: boolean
+  className?: string
 }
 
-export function Text({ size = 'md', children, asChild }: TextProps) {
-  const Comp = asChild ? Slot : 'span';
+export function Text({ size = 'md', children, asChild, className }: TextProps) {
+  const Comp = asChild ? Slot : 'span'
 
   return (
     <Comp
@@ -18,7 +19,8 @@ export function Text({ size = 'md', children, asChild }: TextProps) {
           'text-xs': size === "sm",
           'text-sm': size === 'md',
           'text-md': size === "lg",
-        }
+        },
+        className
       )}
     >
       {children}
